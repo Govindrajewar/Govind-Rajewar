431e17604b26f319a65d06d2af0d6c99a200d68b
Govind Rajewar <abhirajewar87@gmail.com>
2025-06-13 22:03:23 +0530

    Fixed Footer alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,15 @@
-import React from "react";
-import "../style/Footer.css"; // Import CSS file
+import "../style/Footer.css";
 
 function Footer() {
   return (
     <footer className="footer">
       <p>© 2025 Govind Rajewar | All rights reserved.</p>
       <div className="footer-links">
-        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/Govindrajewar" target="_blank" rel="noopener noreferrer">
           GitHub
-        </a>{" "}
-        |
-        <a
-          href="https://linkedin.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
+        </a>
+        <a href="https://www.linkedin.com/in/govind-rajewar/" target="_blank" rel="noopener noreferrer">
           LinkedIn
-        </a>{" "}
-        |
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Twitter
         </a>
       </div>
     </footer>
